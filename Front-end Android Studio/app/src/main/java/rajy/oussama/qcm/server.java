7b576bf496ad48bb61a4c1203194e45a5f0502be
Oussama Rajy <84742676+oussamarajy@users.noreply.github.com>
2022-12-11 21:16:49 +0100

    Update server.java

--- Front-end Android Studio/app/src/main/java/rajy/oussama/qcm/server.java
+++ Front-end Android Studio/app/src/main/java/rajy/oussama/qcm/server.java
@@ -1,5 +1,5 @@
 package rajy.oussama.qcm;
 
 public class server {
-    public static String Url_server = "http://mouhammedx110.pythonanywhere.com/";
+    public static String Url_server = "http://oussamarajy.pythonanywhere.com/";
 }
